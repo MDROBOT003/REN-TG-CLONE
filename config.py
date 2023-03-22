84ef6e67d790f49a54d4cb1aae16a787c63c13cc
MDROBOT003 <120124913+MDROBOT003@users.noreply.github.com>
2023-03-22 18:21:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "0d3f74056f1e60388d3317548799ee17")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "1BVtsOHYBu280uIV9rEwDTwVqL4JxHqBtua0E4ctjyu51KOgXXqZTGS0ou45mfY6_EdM6IEZkz-K_5B63Hl8D9dlq7c3-HU3I-rFzqHzoIY6EYsMg2LNDs92DofKM358X5i_dSrw1eoSQPCbuNS7EreQpyJaeBKGgrWFJG-j4j3ZC8Ccoa6iIkP3r4aM1Yvvgu7vij13gXbC8thdhcECaBPmL5MRARYMW6aigQAI36zCxdhHLf9USDnCqOtW7A4OR-pzRrvIGPZny967DNDs_FNnWMghLovVwJBOkkSb7LiW9sQLtxNLvUTiwjCzergQTWTg7MnliAptjAYhTrdMIjVv_BnIDenE=")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQCWNWIAmNyqTWg-IlWwXYQzd4AUW_wpOLY6vr9SDEipXSNnyeockeTuh_Db2B0GVsiowwLNX5JOVBaR3mKRgKU6wFmViqmJNbATx-OfcS6-mwZFUF-Up0bl_UA94tnKWpwZ0_GKc6Zn7w9DorAHX3J6J1unDQoBkdcLRbXDk9HxbnBA5kdq3VrGI3wRAsLjQG9C-G_FoY3j53SC1LlyadcKuY5dkfLc4AjqHVP7rKumW7LBMHF0fDqPf0ChbkJcFURXc3DfUdkedC8GoAcIDLPmLdISBRta4hxTr2Kvkqz6fMcDHcAhhheDk5a87gVMw-tL4tZi6Lz-C0dSvWmseBxx4U9PcwAAAABphwJ4AA")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "postgres://fileforward_xgrl_user:kkmQxLQoxea8Y6h2AnFaM80ege5Yv6Dd@dpg-cgcsbo64dad6fr6t3k6g-a/fileforward_xgrl")
